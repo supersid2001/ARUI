28d2c9e1d44dfed44ff63d57aed3625d6bde25e3
sidd <supersid2001@gmail.com>
2023-10-27 22:34:34 -0400

    Updated TaskListPositionManager with rotation algorithm

--- Assets/Plugins/ARUI/Scripts/TaskList/Sid_Scripts/Placement and Orientation/TasklistPositionManager.cs
+++ Assets/Plugins/ARUI/Scripts/TaskList/Sid_Scripts/Placement and Orientation/TasklistPositionManager.cs
@@ -14,8 +14,9 @@ public class TasklistPositionManager : Singleton<TasklistPositionManager>
 
     public float movementSpeed = 1.0f;
     //Offset from camera if no objects exist
-    public float xOffset;
+    public float xOffset = 0.0f;
     public float zOffset;
+    public float minDistance = 0.1f;
 
     #region Delay Values
     //public float xOffset;
@@ -26,7 +27,7 @@ public class TasklistPositionManager : Singleton<TasklistPositionManager>
     //Vector3 LastPosition;
     //float CurrDelay;
     #endregion
-    public float heightOffset;
+    public float heightOffset = 0.4f;
 
     Dictionary<string, GameObject> objsDict = new Dictionary<string, GameObject>();
     Dictionary<string, GameObject> linesDict = new Dictionary<string, GameObject>();
@@ -38,14 +39,29 @@ public class TasklistPositionManager : Singleton<TasklistPositionManager>
 
     float timeStarted;
 
+    [SerializeField]
+    float distanceThreshold = 1.0f;
+    [SerializeField]
+    float distanceTimeThreshold = 5.0f;
+    [SerializeField]
+    int numDivisions = 20;
+    [SerializeField]
+    float rotationTimeThreshold = 5.0f;
+    float divisionDifference;
+    Vector3 setPosition;
+    Vector2 currBounds;
+    Vector2 setBounds;
+    float rotationTimer = 0.0f;
+    float positionTimer = 0.0f;
+
     bool isLooking = false;
     #region Delay Code
     // Start is called before the first frame update
-    void Start()
+/*    void Start()
     {
         _listContainer = transform.GetChild(1).gameObject;
         //LastPosition = Camera.main.transform.position;
-    }
+    }*/
     #endregion
     //Function to have the task overview snap to the center of all required objects
     //This is done when the recipe goes from one step to another. By default, the overview
@@ -54,23 +70,67 @@ public class TasklistPositionManager : Singleton<TasklistPositionManager>
     {
         Vector3 centroid = Camera.main.transform.position + Camera.main.transform.forward * zOffset + Camera.main.transform.right * xOffset;
         Vector3 finalPos = new Vector3(centroid.x, Camera.main.transform.position.y + heightOffset, centroid.z);
+        float DistanceToCam = finalPos.GetDistanceToSpatialMap();
+        if(DistanceToCam > 0 && DistanceToCam < zOffset)
+        {
+            float currOffset = Mathf.Max(minDistance, DistanceToCam);
+            centroid = Camera.main.transform.position + Camera.main.transform.forward * currOffset + Camera.main.transform.right * xOffset;
+            finalPos = new Vector3(centroid.x, Camera.main.transform.position.y + heightOffset, centroid.z);
+        };
         BeginLerp(this.transform.position, finalPos);
+    }
+
+    void Start()
+    {
+        divisionDifference = 360.0f / numDivisions;
+        setBounds = new Vector2(0.0f, divisionDifference);
+        setPosition = Camera.main.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (MultiTaskList.Instance == null)
-            return;
-
-        MultiTaskList.Instance.SetLineStart(_listContainer.transform.position);
-        Vector3 centroid = new Vector3(0, 0, 0);
-        foreach (KeyValuePair<string, GameObject> pair in objsDict)
+        UnityEngine.Debug.Log("Current Rotation: " + setBounds);
+        UnityEngine.Debug.Log("Current Position: " + setPosition);
+        float dist = Vector3.Distance(Camera.main.transform.position, setPosition);
+        //MANAGING ROTATION
+        float currAngle = Clamp0360(Camera.main.transform.eulerAngles.y);
+        float lowerBound = currAngle - (currAngle % divisionDifference);
+        float upperBound = lowerBound + divisionDifference;
+        Vector2 newBounds = new Vector2(lowerBound, upperBound);
+        if (currBounds != newBounds)
         {
-            centroid += pair.Value.transform.position;
+            currBounds = newBounds;
+            rotationTimer = 0.0f;
         }
-        centroid = centroid / objsDict.Count;
-        MultiTaskList.Instance.SetLineEnd(centroid);
+        if (currBounds == setBounds && dist < distanceThreshold)
+        {
+            //ACTIVATE INDICATOR??
+        }
+        if (currBounds != setBounds)
+        {
+            rotationTimer += Time.deltaTime;
+        }
+        if (rotationTimer >= rotationTimeThreshold)
+        {
+            rotationTimer = 0.0f;
+            setBounds = currBounds;
+            SnapToCentroid();
+        }
+        //MANAGING POSITION
+        if (dist >= distanceThreshold)
+        {
+            positionTimer += Time.deltaTime;
+        }
+        else
+        {
+            positionTimer = 0.0f;
+        }
+        if (positionTimer >= distanceTimeThreshold)
+        {
+            setPosition = Camera.main.transform.position;
+            SnapToCentroid();
+        }
     }
 
     //Source -> https://www.blueraja.com/blog/404/how-to-use-unity-3ds-linear-interpolation-vector3-lerp-correctly
@@ -104,6 +164,16 @@ public class TasklistPositionManager : Singleton<TasklistPositionManager>
     public void SetIsLooking(bool val)
     {
         isLooking = val;
+    }
+
+    public float Clamp0360(float eulerAngles)
+    {
+        float result = eulerAngles - Mathf.CeilToInt(eulerAngles / 360f) * 360f;
+        if (result < 0)
+        {
+            result += 360f;
+        }
+        return result;
     }
 
 
